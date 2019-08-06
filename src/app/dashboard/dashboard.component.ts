f1d03484de61196cbe167d6d36ae32646cb3b520
Ygor Magalhaes <emaildoygor@gmail.com>
2019-08-06 17:59:19 -0300

    Exemplo de gráfico

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,3 +6,41 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
 
 }
+
+/*
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'variablepie'
+    },
+    series: [{
+        minPointSize: 10,
+        innerSize: '20%',
+        zMin: 0,
+        name: 'countries',
+        data: [{
+            name: 'Poland',
+            y: 20,
+            z: 124.6
+        }, {
+            name: 'Czech Republic',
+            y: 20,
+            z: 137.5
+        }, {
+            name: 'Italy',
+            y: 20,
+            z: 201.8
+        }, {
+            name: 'Switzerland',
+            y: 20,
+            z: 214.5
+        }, {
+            name: 'Germany',
+            y: 20,
+            z: 235.6
+        }]
+    }]
+});
+
+
+*/
